a05117c710cc704ebd1617938d027760d85bbc99
Ramjan75 <ramjanplus5@gmail.com>
2021-11-05 15:04:20 +0600

    fsd ramjan

---
+++ android/app/src/main/kotlin/com/epanda/epanda_ecommerce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.epanda.epanda_ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/other_config.dart
@@ -0,0 +1,6 @@
+
+class OtherConfig{
+  static const bool USE_PUSH_NOTIFICATION = false;
+  static const bool USE_GOOGLE_MAP = true;
+  static const String GOOGLE_MAP_API_KEY = "AIzaSyAhYULqbwma8p0eCus-ifHmnY-efSKY8ZE";
+}
